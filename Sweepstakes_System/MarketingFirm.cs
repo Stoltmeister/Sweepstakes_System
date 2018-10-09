dd3187baca2ebe29f6d91ac8e2b0caad54a7e5e3
Joshua Stoltenberg <stoltenberg96@gmail.com>
2018-10-09 15:24:01 -0500

    Attempting to use factory design inside the MarketingFirm class by allowing the user to choose between a stack or queue manager.

--- Sweepstakes_System/MarketingFirm.cs
+++ Sweepstakes_System/MarketingFirm.cs
@@ -8,5 +8,19 @@ namespace Sweepstakes_System
 {
     class MarketingFirm
     {
+        public static ISweepstakesManager GetManager()
+        {
+            switch (UserInterface.GetInput("Enter '1' if you would like to use the StackManager or '2' if you would like to use the QueueManager \n"))
+            {
+                case "1": SweepstakesStackManager stackSweep = new SweepstakesStackManager();
+                    return stackSweep;
+                case "2": SweepstakesQueueManager queueSweep = new SweepstakesQueueManager();
+                    return queueSweep;
+                default:
+                    // input checking?
+                    return new SweepstakesStackManager();
+            }
+        }
+        
     }
 }
